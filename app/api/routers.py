4602382be027e5c846f9418eae259de0a9a5e6a1
Семенова Елизавета <thealeeeaaa23@gmail.com>
2024-01-28 03:47:43 +0300

    h

--- app/api/routers.py
+++ app/api/routers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.api.endpoints import (charity_project_router, donation_router,
                                user_router)
+from app.api.endpoints.google_api import router
 
 main_router = APIRouter()
 main_router.include_router(
@@ -9,5 +10,8 @@ main_router.include_router(
 )
 main_router.include_router(
     donation_router, prefix='/donation', tags=['donations']
+)
+main_router.include_router(
+    router, prefix='/google', tags=['Google']
 )
 main_router.include_router(user_router)
